aeca3c72bfc1ae3d54a11ab03f31ab68aaa1d1c4
pramebt <bdforwk@gmail.com>
2025-04-17 11:19:03 +0700

    first commit

---
+++ src/components/Contactform.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contactform = () => {
+  return (
+    <div className= 'bg-white w-[1000px] h-[800px] p-10 shadow-2xl rounded-2xl'>
+      <h1 className='font-bold text-[32px]'>Contact us</h1>
+    </div>
+  )
+}
+
+export default Contactform
